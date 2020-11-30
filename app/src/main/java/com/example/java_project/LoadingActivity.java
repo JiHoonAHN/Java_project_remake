caf8484582e6fe9f64edccbc6e6d8b32a1eb629f
sws2 <ahnjh1028@naver.com>
2020-11-30 15:09:53 +0900

    Java_Project remake

--- app/src/main/java/com/example/java_project/LoadingActivity.java
+++ app/src/main/java/com/example/java_project/LoadingActivity.java
@@ -12,17 +12,17 @@ public class LoadingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
         Handler hd = new Handler();
-        hd.postDelayed(new LoadingActivityhandler(), 3000); // 1초 후에 hd handler 실행  3000ms = 3초
+        hd.postDelayed(new LoadingActivityhandler(), 3000); // 1초 뒤 hd handler 실행
     }
 
     private class LoadingActivityhandler implements Runnable {
         public void run(){
-            startActivity(new Intent(getApplication(), MainActivity.class)); //로딩이 끝난 후, ChoiceFunction 이동
+            startActivity(new Intent(getApplication(), MainActivity.class)); //로딩후, ChoiceFunction 이동
             LoadingActivity.this.finish(); // 로딩페이지 Activity stack에서 제거
         }
     }
     @Override
     public void onBackPressed() {
-        //초반 플래시 화면에서 넘어갈때 뒤로가기 버튼 못누르게 함
+        //초반 뒤로가기 버튼 못누르게 함
     }
 }
--- app/src/main/java/com/example/java_project/MainActivity.java
+++ app/src/main/java/com/example/java_project/MainActivity.java
@@ -2,16 +2,14 @@ package com.example.java_project;
 
 
 import androidx.appcompat.app.AppCompatActivity;
-import java.util.Date;
 import java.text.SimpleDateFormat;
-import java.util.Locale;
-
 import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.TextView;
-
+//1. 주석 사용
+//2. 매서드 이해
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView_Date;
@@ -20,26 +18,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.InitializeView();
-        this.InitializeListener();
+        this.View();
+        this.Listener();
     }
-    public void InitializeView()
+    public void View()
     {
         textView_Date = (TextView)findViewById(R.id.textView_date);
     }
-    public void InitializeListener()
+    public void Listener()
     {
         callbackMethod = new DatePickerDialog.OnDateSetListener()
         {
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth)
             {
+                monthOfYear +=1;
                 textView_Date.setText(year + "년" + monthOfYear + "월" + dayOfMonth + "일");
             }
         };
     }
-
+    // month는 0 부터 시작
     public void OnClickHandler(View view) {
+
+        SimpleDateFormat year_1 = new SimpleDateFormat("yyyy");
+        SimpleDateFormat month_1 = new SimpleDateFormat("MM");
+        SimpleDateFormat day_1 = new SimpleDateFormat("dd");
 
         DatePickerDialog dialog = new DatePickerDialog(this, callbackMethod, 2020, 11, 29);
         dialog.show();
