08fa9dc0d4057e9124de7ca71132a369acf657e8
sws2 <ahnjh1028@naver.com>
2020-12-03 10:25:05 +0900

    Java_Project remake_완성본

--- app/src/main/java/com/example/java_project/MainActivity.java
+++ app/src/main/java/com/example/java_project/MainActivity.java
@@ -9,31 +9,26 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity { //MainActivity 는 Activity를 상속한다.
-
     DatePicker datePicker;  //  datePicker - 날짜를 선택하는 달력
     TextView viewDatePick;  //  viewDatePick - 선택한 날짜를 보여주는 textView
     EditText edt;   //  edtDiary - 선택한 날짜의 내용을 쓰거나 기존에 저장된 내용이 있다면 보여주고 수정가능한 영역
     Button btnSave;   //  btnSave - 선택한 날짜의 일기 저장 및 수정(덮어쓰기) 버튼
     String fileName;   //  fileName - 돌고 도는 선택된 날짜의 파일 이름
-
     @Override
     protected void onCreate(Bundle savedInstanceState) { // 앱 첫 시작 시 돌아감
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // 뷰에 있는 위젯들 리턴 받아두기
+        // 뷰에 있는 위젯들 리턴 받아 둠
         datePicker = (DatePicker) findViewById(R.id.datePicker); //calendar 내용을 가져온다.
         viewDatePick = (TextView) findViewById(R.id.viewDatePick);//날짜를 보여준다.
         edt = (EditText) findViewById(R.id.edt);//내용을 입력 받을 부분
-        btnSave = (Button) findViewById(R.id.btnSave);// 버튼 -> save 하는 버튼
+        btnSave = (Button) findViewById(R.id.btnSave);// 버튼 -> 기능: save 하는 버튼
 
         // 오늘 날짜를 받게해주는 Calender
         Calendar c = Calendar.getInstance();
@@ -59,7 +54,7 @@ public class MainActivity extends AppCompatActivity { //MainActivity 는 Activit
             @Override
             public void onClick(View v) {
                 // fileName을 넣고 저장 시키는 메소드를 호출
-                saveDiary(fileName);
+                save(fileName);
             }
         });
     }
@@ -98,7 +93,7 @@ public class MainActivity extends AppCompatActivity { //MainActivity 는 Activit
 
     // 내용 저장하는 메소드
     @SuppressLint("WrongConstant")//->@SuppressLint 어노테이션을 사용하며. 인자로 검사에서 제외할 항목 ID를 입력
-    private void saveDiary(String readDay) {
+    private void save(String readDay) {
 
         FileOutputStream fos = null; //FileOutputStream 파일로 바이트 단위의 출력을 내보내는 클래스
 
